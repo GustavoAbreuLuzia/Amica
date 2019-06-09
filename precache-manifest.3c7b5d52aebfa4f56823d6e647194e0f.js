52d4e87198f219d1396a0cd9af99432f11ac5b73
Gustavo Luzia <gustavo.abreuluzia@gmail.com>
2019-06-09 22:06:15 +0100

    Updates

---
+++ precache-manifest.3c7b5d52aebfa4f56823d6e647194e0f.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "5434f779d7e71ed814d29fd1e25dbff7",
+    "url": "/Amica/static/media/alf (3).5434f779.jpg"
+  },
+  {
+    "revision": "91b50414d6a4ed76362b",
+    "url": "/Amica/static/css/main.54eea6a0.chunk.css"
+  },
+  {
+    "revision": "4c0c97fafc9a5155eda7",
+    "url": "/Amica/static/js/runtime~main.4c0c97fa.js"
+  },
+  {
+    "revision": "8a4df1612772b98efc15",
+    "url": "/Amica/static/js/2.8a4df161.chunk.js"
+  },
+  {
+    "revision": "dc14e7568bc38f1d35c3ae9ae5dbf3cf",
+    "url": "/Amica/static/media/alf.dc14e756.jpg"
+  },
+  {
+    "revision": "904992325c78202b67c3d94db28441c2",
+    "url": "/Amica/static/media/gatinha (2).90499232.jpg"
+  },
+  {
+    "revision": "6b1538091ddd0deac1d4e4739558e6c5",
+    "url": "/Amica/static/media/caoEncontrado.6b153809.jpg"
+  },
+  {
+    "revision": "8985561d49b1f63a166a55fd4a42ee73",
+    "url": "/Amica/static/media/Sky.8985561d.jpg"
+  },
+  {
+    "revision": "3aa74fb887c1842b4044ef1abfb230bc",
+    "url": "/Amica/static/media/catDog.3aa74fb8.svg"
+  },
+  {
+    "revision": "a59bd47dc3cad1760ed4a0c9be251031",
+    "url": "/Amica/static/media/Gato1.a59bd47d.jpg"
+  },
+  {
+    "revision": "fa71001c5e6205ba6dac57312bc01a14",
+    "url": "/Amica/static/media/GatoBranco.fa71001c.jpg"
+  },
+  {
+    "revision": "5c81d7a9a1068329748ecc64f660986a",
+    "url": "/Amica/static/media/Pepe.5c81d7a9.jpg"
+  },
+  {
+    "revision": "e982180543cd4df8490ea89299d8ffbd",
+    "url": "/Amica/static/media/Collie.e9821805.jpg"
+  },
+  {
+    "revision": "ca9c2c6cfab9e45bf2558c77b4d6806f",
+    "url": "/Amica/static/media/Julieta.ca9c2c6c.jpg"
+  },
+  {
+    "revision": "c98b9ed163428691cce0fa03aa3b732c",
+    "url": "/Amica/static/media/Foxy.c98b9ed1.jpg"
+  },
+  {
+    "revision": "333cad38707edfec3ca039af19e15d54",
+    "url": "/Amica/static/media/gatinha (1).333cad38.jpg"
+  },
+  {
+    "revision": "084335b21686fa408fea0c73d324622a",
+    "url": "/Amica/static/media/gatinha (3).084335b2.jpg"
+  },
+  {
+    "revision": "29f2c413437a2762138d23b7228fef14",
+    "url": "/Amica/static/media/alf (1).29f2c413.jpg"
+  },
+  {
+    "revision": "aff8da6f0cda7e4f5518647297bf67a4",
+    "url": "/Amica/static/media/alf (2).aff8da6f.jpg"
+  },
+  {
+    "revision": "91b50414d6a4ed76362b",
+    "url": "/Amica/static/js/main.91b50414.chunk.js"
+  },
+  {
+    "revision": "f27e80b05510faa2578a75b6d31cb091",
+    "url": "/Amica/static/media/sky (1).f27e80b0.jpg"
+  },
+  {
+    "revision": "a4461e335a7dc8c0d24bce98353740fd",
+    "url": "/Amica/static/media/sky (2).a4461e33.jpg"
+  },
+  {
+    "revision": "eab522e193e5448eaf54424440598dcb",
+    "url": "/Amica/static/media/avVeterinaria.eab522e1.png"
+  },
+  {
+    "revision": "ac3e6a725f5be7b96b98fe8298f9a609",
+    "url": "/Amica/static/media/briFitness.ac3e6a72.png"
+  },
+  {
+    "revision": "78edd28c27d50f8b001c5323a66e2bca",
+    "url": "/Amica/static/media/brigoffice.78edd28c.png"
+  },
+  {
+    "revision": "a25a123db73d8b0f38c4169907655d26",
+    "url": "/Amica/static/media/chamauto.a25a123d.png"
+  },
+  {
+    "revision": "a8dc72d3ca524eba04f9708e9e526db9",
+    "url": "/Amica/static/media/clinicaValeAlva.a8dc72d3.png"
+  },
+  {
+    "revision": "62168b10bf1bfa921e8359002a4ccfde",
+    "url": "/Amica/static/media/donaFlorinda.62168b10.png"
+  },
+  {
+    "revision": "1429043ecfa89e20bf31b691e24b04de",
+    "url": "/Amica/static/media/fisioSaude.1429043e.png"
+  },
+  {
+    "revision": "0beefe411001caa1687570b88a708e05",
+    "url": "/Amica/static/media/novavet.0beefe41.png"
+  },
+  {
+    "revision": "c2a715b875748cb805662740b483d3a4",
+    "url": "/Amica/static/media/quintaDasQueimadas.c2a715b8.png"
+  },
+  {
+    "revision": "7b1a09f2f1accb799cb33d262d6f6ded",
+    "url": "/Amica/static/media/senhoraPereiras.7b1a09f2.png"
+  },
+  {
+    "revision": "2b838681425db6074bc62602b5c53d44",
+    "url": "/Amica/static/media/tascaNoz.2b838681.png"
+  },
+  {
+    "revision": "75e1cc7a9f91ad5e3951ad7da404de06",
+    "url": "/Amica/static/media/vetCantarias.75e1cc7a.png"
+  },
+  {
+    "revision": "ee1ff55b672fa60db9ae6cb75e72f08d",
+    "url": "/Amica/static/media/vetSantiago.ee1ff55b.png"
+  },
+  {
+    "revision": "dab20e1497c5a82ef6ae86ae9e7804c0",
+    "url": "/Amica/static/media/logoTransparent.dab20e14.png"
+  },
+  {
+    "revision": "87d8537e1824d654a02cd4aa85bcbcf8",
+    "url": "/Amica/static/media/header.87d8537e.jpg"
+  },
+  {
+    "revision": "a72fd228f47d3ead467b6feafb50c152",
+    "url": "/Amica/index.html"
+  }
+];
